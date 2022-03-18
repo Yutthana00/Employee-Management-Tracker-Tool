ced64a9a39f58bc60238c52d966aa900c604150c
Yutthana00 <arf101293@gmail.com>
2022-03-18 03:25:54 +0000

    salary and department id added in black gaps

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,17 +5,17 @@ VALUE   ("Engineering"),
         ("Customer service"),
         ("Legal");
 
-INSERT INTO role (titlem salary, department_id)
-VALUES  ("Engineering Manager", == , ==),
-        ("Software Engineer", ==, ==),
-        ("Sales Manager", ==, ==),
-        ("Sales", ==, ==),
-        ("Customer Service Manager", ==, ==),
-        ("Custoemr Service Support", ==, ==),
-        ("Finance Manager", ==, ==),
-        ("Accountant", ==, ==),
-        ("Legal Team Manager", ==, ==),
-        ("Lawyer", ==, ==);
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Engineering Manager", 95000 , 2001),
+        ("Software Engineer", 40000, 2001),
+        ("Sales Manager", 80000, 2002),
+        ("Sales", 35000, 2002),
+        ("Customer Service Manager", 75000, 2003),
+        ("Customer Service Support", 24000, 2003),
+        ("Finance Manager", 89000, 2004),
+        ("Accountant", 55000, 2004),
+        ("Legal Team Manager", 85000, 2005),
+        ("Lawyer", 55000, 2005);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
