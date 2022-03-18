e580ae84ca146475ef2762a4720265cc9afa0c74
Yutthana00 <arf101293@gmail.com>
2022-03-18 03:13:34 +0000

    seeds template

---
+++ db/seeds.sql
@@ -0,0 +1,39 @@
+INSERT INTO deparment (name)
+VALUE   ("Engineering"),
+        ("Sales"),
+        ("Finance"),
+        ("Customer service"),
+        ("Legal");
+
+INSERT INTO role (titlem salary, department_id)
+VALUES  ("Engineering Manager", == , ==),
+        ("Software Engineer", ==, ==),
+        ("Sales Manager", ==, ==),
+        ("Sales", ==, ==),
+        ("Customer Service Manager", ==, ==),
+        ("Custoemr Service Support", ==, ==),
+        ("Finance Manager", ==, ==),
+        ("Accountant", ==, ==),
+        ("Legal Team Manager", ==, ==),
+        ("Lawyer", ==, ==);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("", "", ,NULL),
+("", "", ,NULL),
+("", "", ,NULL),
+("", "", ,NULL),
+("", "", ,NULL),
+("", "", , ),
+("", "", , ),
+("", "", , ),
+("", "", , ),
+("", "", , ),
+("", "", , ),
+("", "", , ),
+("", "", , ),
+("", "", , ),
+("", "", , ),
+("", "", , ),
+
+        
